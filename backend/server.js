eff1ad2a37e93f3e46213b121c173e4dbab32767
Mehanth <mehanth2310579@ssn.edu.in>
2025-06-11 14:56:23 +0530

    updated server to listen on 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -226,6 +226,6 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(3001, () => {
-  console.log('Server running on https://localhost:3001');
+server.listen(3001, '0.0.0.0', () => {
+  console.log('Server running on port 3001');
 });
